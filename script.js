c4d1c7a1d403ac18681b8d16a611e3cbfcb01f4d
yusran jonjo <msaidibrahim11@gmail.com>
2021-03-03 17:09:45 +0700

    15. menambahkan file script.js

---
+++ script.js
@@ -0,0 +1,58 @@
+const barsMenu = document.querySelector('.bars-menu');
+const navbar = document.querySelector('.navbar');
+const daftarMenu = document.querySelector('.daftar-menu');
+const header = document.querySelector('.header');
+
+const a = document.querySelectorAll('.navbar ul li a');
+const iconBars = document.querySelector('.navbar .bars-menu i');
+
+const li = document.querySelectorAll('.navbar ul li');
+const li1 = document.querySelector('.navbar ul li:nth-child(1)');
+const li2 = document.querySelector('.navbar ul li:nth-child(2)');
+const li3 = document.querySelector('.navbar ul li:nth-child(3)');
+
+li1.addEventListener('click', function () {
+    let dm = daftarMenu;
+    let ib = iconBars;
+    dm.classList.toggle('menus');
+    dm.classList.toggle('daftar-menu');
+    ib.classList.toggle('fa-times');
+    dm.classList.toggle('menus');
+});
+
+
+li2.addEventListener('click', function () {
+    let dm = daftarMenu;
+    let ib = iconBars;
+    dm.classList.toggle('menus');
+    dm.classList.toggle('daftar-menu');
+    ib.classList.toggle('fa-times');
+    dm.classList.toggle('menus');
+});
+
+li3.addEventListener('click', function () {
+    let dm = daftarMenu;
+    let ib = iconBars;
+    dm.classList.toggle('menus');
+    dm.classList.toggle('daftar-menu');
+    ib.classList.toggle('fa-times');
+    dm.classList.toggle('menus');
+});
+
+
+barsMenu.addEventListener('click', btnShowMenu);
+
+function btnShowMenu() {
+    for (let i = 0; i < li.length; i++) {
+        li[i].classList.add('li-menu');
+    }
+
+    for (let i = 0; i < a.length; i++) {
+        a[i].classList.add('a-menu');
+    }
+    daftarMenu.classList.toggle('daftar-menu');
+    daftarMenu.classList.add('menus');
+    iconBars.classList.toggle('fa-times');
+
+    // barsMenu.innerHTML = '<i class="fas fa-times"></i>';
+}
